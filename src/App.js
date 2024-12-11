e62233de752b86361a27f9eaee1ed086f8a16e33
amymcevoy <G00413220@atu.ie>
2024-12-11 16:02:29 +0000

    Fixed routing for AddMeal page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 
 //components
 import Home from './pages/Home'; // Import Home Page
+import AddMeal from './pages/AddMeal';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />  
+        <Route path="/" element={<Home />} /> 
+        <Route path="/add-meal" element={<AddMeal />} /> 
       </Routes>
     </Router>
   );
