3012c14ff657469f0ae8dcd798e2784cc32450cb
amymcevoy <G00413220@atu.ie>
2024-12-11 15:34:26 +0000

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // For navigation between pages
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to PrepPal!</h1>
+    </div>
+  );
+}
+
+export default Home;
